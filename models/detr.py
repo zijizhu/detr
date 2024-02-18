5de9be5979ee036680d8d4ef9d989847b9ed4306
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-02-18 16:41:36 +1100

    modifies detr to return decoder outputs

--- models/detr.py
+++ models/detr.py
@@ -69,7 +69,7 @@ class DETR(nn.Module):
         out = {'pred_logits': outputs_class[-1], 'pred_boxes': outputs_coord[-1]}
         if self.aux_loss:
             out['aux_outputs'] = self._set_aux_loss(outputs_class, outputs_coord)
-        return out
+        return hs, out
 
     @torch.jit.unused
     def _set_aux_loss(self, outputs_class, outputs_coord):
